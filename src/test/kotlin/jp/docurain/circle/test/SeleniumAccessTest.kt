9c06598a814759a4ae0b739f15638ed0c4844792
withpop <11686989+withpop@users.noreply.github.com>
2019-07-05 16:40:27 +0900

    add test

---
+++ src/test/kotlin/jp/docurain/circle/test/SeleniumAccessTest.kt
@@ -0,0 +1,15 @@
+package jp.docurain.circle.test
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class SeleniumAccessTest {
+    @Test
+    fun failTest(){
+        fail("This test will always fail.")
+    }
+
+    @Test
+    fun successTest(){
+    }
+}
