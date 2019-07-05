005dde73d68c08cabd81fdcb447c879cf9b3f625
withpop <11686989+withpop@users.noreply.github.com>
2019-07-05 17:36:42 +0900

    add test

--- src/test/kotlin/jp/docurain/circle/test/SeleniumAccessTest.kt
+++ src/test/kotlin/jp/docurain/circle/test/SeleniumAccessTest.kt
@@ -2,8 +2,26 @@ package jp.docurain.circle.test
 
 import org.junit.Assert.*
 import org.junit.Test
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeDriverService
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.By
+
+
 
 class SeleniumAccessTest {
+
+    private val driver: ChromeDriver
+    private val options: ChromeOptions
+    init {
+        System.setProperty("webdriver.chrome.driver", "chromeDriverPath")
+        options = ChromeOptions()
+        options.setBinary("/usr/bin/google-chrome")
+        options.addArguments("--headless")
+        val driverService = ChromeDriverService.createDefaultService()
+        driver = ChromeDriver(driverService, options)
+    }
+
     @Test
     fun failTest(){
         fail("This test will always fail.")
@@ -11,5 +29,17 @@ class SeleniumAccessTest {
 
     @Test
     fun successTest(){
+        driver.get("https://docurain.jp/")
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='今すぐ無料トライアル'])[3]/following::a[1]"))
+            .click()
+        try {
+            assertEquals(
+                "ドキュレインに関するお問い合わせ",
+                driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='ログイン'])[2]/following::h2[1]")).text
+            )
+        } catch (e: Error) {
+            fail("exception thrown:" + e.message)
+        }
+
     }
 }
